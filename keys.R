366f03dee76e4f31d43468cea1a36df529597ee5
Meredith Winchell <meredithwwinchell@gmail.com>
2015-01-08 14:59:13 -0500

    Create and set up ecigs project

---
+++ keys.R
@@ -0,0 +1,6 @@
+consumer_key <- "lqYTMVaQSG07vwrTR8entaUOz"
+consumer_secret <- "tr5yWgVOXCoyRE11hfvvuYG0Kpug2JjlfNd7bxjJI6byZKtCUm"
+access_token <- "66726553-dJOEptAV6ZuZVbD9PM33hwCWd01dJfpG8N0nzgyKp"
+access_token_secret <- "hlTIDL04WwaTqxsooLCuPkqlAiUVMRCBEguYNqqSOfBUx"
+library(twitteR)
+setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_token_secret)
