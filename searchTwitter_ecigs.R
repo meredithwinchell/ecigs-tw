dacb1907d413dd5fde912eb348ef85c6d71496ab
Meredith Winchell <meredithwwinchell@gmail.com>
2015-01-24 11:58:30 -0500

    Correct typos

--- searchTwitter_ecigs.R
+++ searchTwitter_ecigs.R
@@ -1,4 +1,4 @@
 # get tweets with a hashtag -> for date since = n, until = n + 1d
-tweets <- searchTwitter("#ecig OR ecig OR vape OR #vape OR 'esmoking'", n=10000, since="2015-01-0", until="2015-01-02")
+tweets <- searchTwitter("#ecig OR ecig OR vape OR #vape OR 'esmoking'", n=10000, since="2015-01-01", until="2015-01-02")
 # save data
 save(tweets,file="getTweets_df_20150101.Rdata")
